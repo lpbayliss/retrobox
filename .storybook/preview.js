0b3c2f6bbacf2a9d10f9cc0ac84e02f29a245000
Luke Bayliss <lukepbayliss@gmail.com>
2022-09-13 02:02:03 +1000

    adds storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import theme from "../theme/theme.ts";
+import { reactIntl } from "./reactIntl.js";
+
+console.log(reactIntl.messages.length)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  chakra: {
+    theme,
+  },
+  reactIntl,
+  locale: reactIntl.defaultLocale,
+  locales: {
+    'en-AU': "English",
+  },
+};
---
+++ components/card/card.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Card from "./card.component";
+
+export default {
+  title: "Components / Card",
+  component: Card,
+  argTypes: {},
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
