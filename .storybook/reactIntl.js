c8a32ec20287291c9fb9fb41fdfb29f7acd01e7a
Luke Bayliss <lukepbayliss@gmail.com>
2022-09-13 02:02:03 +1000

    adds storybook

---
+++ .storybook/reactIntl.js
@@ -0,0 +1,15 @@
+const locales = ['en-AU'];
+
+const messages = locales.reduce((acc, lang) => ({
+  ...acc,
+  [lang]: require(`../i18n/locales/${lang}.json`), // whatever the relative path to your messages json is
+}), {});
+
+const formats = {}; // optional, if you have any formats
+
+export const reactIntl = {
+  defaultLocale: 'en-AU',
+  locales,
+  messages,
+  formats,
+};
