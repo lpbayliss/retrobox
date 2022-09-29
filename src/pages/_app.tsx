5541328347de4b22bb24bbd6746fc8faa7d28658
Luke Bayliss <lukepbayliss@gmail.com>
2022-09-29 17:49:08 +1000

    set up client side unleash client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,30 +7,55 @@ import { getMessages } from '@i18n/getMessages';
 import { NextPage } from 'next';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
-import { trpc } from 'src/lib/trpc';
+import { trpc } from '@lib/trpc';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  FlagProvider,
+  IConfig,
+  InMemoryStorageProvider,
+  IToggle,
+  UnleashClient,
+} from '@unleash/proxy-client-react';
 
-export type PageProps = { session: Session };
-
-type MyAppProps<P = {}, IP = P> = AppProps<P> & {
-  Component: NextPage<Omit<P, 'session'>, Omit<IP, 'session'>>;
+const config: IConfig = {
+  url: 'https://retrobox-unleash-proxy-staging.fly.dev/proxy',
+  clientKey: 'staging-proxy',
+  refreshInterval: 15,
+  appName: 'retrobox-development',
+  environment: 'development',
 };
 
-const MyApp: AppType<PageProps> = ({
+const MyApp = ({
   Component,
   pageProps: { session, ...pageProps },
-}: MyAppProps<PageProps>) => {
+}: AppProps<{ session: Session; toggles: IToggle[] }>) => {
   const { locale } = useRouter();
 
+  const ssrOption =
+    typeof window !== 'undefined'
+      ? {}
+      : { fetch: fetch, storageProvider: new InMemoryStorageProvider() };
+
+  const client = new UnleashClient({
+    ...config,
+    ...ssrOption,
+    bootstrap: pageProps.toggles,
+  });
+
+  const toggles = client.getAllToggles();
+  console.log(toggles);
+
   return (
-    <SessionProvider session={session}>
-      <IntlProvider locale={String(locale)} messages={getMessages(String(locale))}>
-        <ChakraProvider resetCSS theme={theme}>
-          <Component {...pageProps} />
-          {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />}
-        </ChakraProvider>
-      </IntlProvider>
-    </SessionProvider>
+    <FlagProvider unleashClient={client}>
+      <SessionProvider session={session}>
+        <IntlProvider locale={String(locale)} messages={getMessages(String(locale))}>
+          <ChakraProvider resetCSS theme={theme}>
+            {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />}
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </IntlProvider>
+      </SessionProvider>
+    </FlagProvider>
   );
 };
 
